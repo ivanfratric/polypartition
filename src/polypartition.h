2ea515c56ae6692b69ffd915a5148af2992ffcb3
Aaron Franke <arnfranke@yahoo.com>
2020-12-23 21:35:34 -0600

    Format code comments to be consistent and proper English

--- src/polypartition.h
+++ src/polypartition.h
@@ -32,11 +32,11 @@ typedef double tppl_float;
 #define TPPL_CCW 1
 #define TPPL_CW -1
 
-//2D point structure
+// 2D point structure.
 struct TPPLPoint {
   tppl_float x;
   tppl_float y;
-  // User-specified vertex identifier.  Note that this isn't used internally
+  // User-specified vertex identifier. Note that this isn't used internally
   // by the library, but will be faithfully copied around.
   int id;
 
@@ -77,7 +77,7 @@ struct TPPLPoint {
   }
 };
 
-//Polygon implemented as an array of points with a 'hole' flag
+// Polygon implemented as an array of points with a "hole" flag.
 class TPPLPoly {
   protected:
   TPPLPoint *points;
@@ -85,14 +85,14 @@ class TPPLPoly {
   bool hole;
 
   public:
-  //constructors/destructors
+  // Constructors and destructors.
   TPPLPoly();
   ~TPPLPoly();
 
   TPPLPoly(const TPPLPoly &src);
   TPPLPoly &operator=(const TPPLPoly &src);
 
-  //getters and setters
+  // Getters and setters.
   long GetNumPoints() const {
     return numpoints;
   }
@@ -125,32 +125,32 @@ class TPPLPoly {
     return points[i];
   }
 
-  //clears the polygon points
+  // Clears the polygon points.
   void Clear();
 
-  //inits the polygon with numpoints vertices
+  // Inits the polygon with numpoints vertices.
   void Init(long numpoints);
 
-  //creates a triangle with points p1,p2,p3
+  // Creates a triangle with points p1, p2, and p3.
   void Triangle(TPPLPoint &p1, TPPLPoint &p2, TPPLPoint &p3);
 
-  //inverts the orfer of vertices
+  // Inverts the orfer of vertices.
   void Invert();
 
-  //returns the orientation of the polygon
-  //possible values:
-  //   TPPL_CCW : polygon vertices are in counter-clockwise order
-  //   TPPL_CW : polygon vertices are in clockwise order
-  //       0 : the polygon has no (measurable) area
+  // Returns the orientation of the polygon.
+  // Possible values:
+  //    TPPL_CCW : Polygon vertices are in counter-clockwise order.
+  //    TPPL_CW : Polygon vertices are in clockwise order.
+  //        0 : The polygon has no (measurable) area.
   int GetOrientation() const;
 
-  //sets the polygon orientation
-  //orientation can be
-  //   TPPL_CCW : sets vertices in counter-clockwise order
-  //   TPPL_CW : sets vertices in clockwise order
+  // Sets the polygon orientation.
+  // Possible values:
+  //    TPPL_CCW : Sets vertices in counter-clockwise order.
+  //    TPPL_CW : Sets vertices in clockwise order.
   void SetOrientation(int orientation);
 
-  //checks whether a polygon is valid or not
+  // Checks whether a polygon is valid or not.
   inline bool Valid() const { return this->numpoints >= 3; }
 };
 
@@ -201,33 +201,33 @@ public:
   typedef std::list<Diagonal> DiagonalList;
 #endif
 
-  //dynamic programming state for minimum-weight triangulation
+  // Dynamic programming state for minimum-weight triangulation.
   struct DPState {
     bool visible;
     tppl_float weight;
     long bestvertex;
   };
 
-  //dynamic programming state for convex partitioning
+  // Dynamic programming state for convex partitioning.
   struct DPState2 {
     bool visible;
     long weight;
     DiagonalList pairs;
   };
 
-  //edge that intersects the scanline
+  // Edge that intersects the scanline.
   struct ScanLineEdge {
     mutable long index;
     TPPLPoint p1;
     TPPLPoint p2;
 
-    //determines if the edge is to the left of another edge
+    // Determines if the edge is to the left of another edge.
     bool operator<(const ScanLineEdge &other) const;
 
     bool IsConvex(const TPPLPoint &p1, const TPPLPoint &p2, const TPPLPoint &p3) const;
   };
 
-  //standard helper functions
+  // Standard helper functions.
   bool IsConvex(TPPLPoint &p1, TPPLPoint &p2, TPPLPoint &p3);
   bool IsReflex(TPPLPoint &p1, TPPLPoint &p2, TPPLPoint &p3);
   bool IsInside(TPPLPoint &p1, TPPLPoint &p2, TPPLPoint &p3, TPPLPoint &p);
@@ -240,137 +240,166 @@ public:
   TPPLPoint Normalize(const TPPLPoint &p);
   tppl_float Distance(const TPPLPoint &p1, const TPPLPoint &p2);
 
-  //helper functions for Triangulate_EC
+  // Helper functions for Triangulate_EC.
   void UpdateVertexReflexity(PartitionVertex *v);
   void UpdateVertex(PartitionVertex *v, PartitionVertex *vertices, long numvertices);
 
-  //helper functions for ConvexPartition_OPT
+  // Helper functions for ConvexPartition_OPT.
   void UpdateState(long a, long b, long w, long i, long j, DPState2 **dpstates);
   void TypeA(long i, long j, long k, PartitionVertex *vertices, DPState2 **dpstates);
   void TypeB(long i, long j, long k, PartitionVertex *vertices, DPState2 **dpstates);
 
-  //helper functions for MonotonePartition
+  // Helper functions for MonotonePartition.
   bool Below(TPPLPoint &p1, TPPLPoint &p2);
   void AddDiagonal(MonotoneVertex *vertices, long *numvertices, long index1, long index2,
           char *vertextypes, std::set<ScanLineEdge>::iterator *edgeTreeIterators,
           std::set<ScanLineEdge> *edgeTree, long *helpers);
 
-  //triangulates a monotone polygon, used in Triangulate_MONO
+  // Triangulates a monotone polygon, used in Triangulate_MONO.
   int TriangulateMonotone(TPPLPoly *inPoly, TPPLPolyList *triangles);
 
   public:
-  //simple heuristic procedure for removing holes from a list of polygons
-  //works by creating a diagonal from the rightmost hole vertex to some visible vertex
-  //time complexity: O(h*(n^2)), h is the number of holes, n is the number of vertices
-  //space complexity: O(n)
-  //params:
-  //   inpolys : a list of polygons that can contain holes
-  //             vertices of all non-hole polys have to be in counter-clockwise order
-  //             vertices of all hole polys have to be in clockwise order
-  //   outpolys : a list of polygons without holes
-  //returns 1 on success, 0 on failure
+  // Simple heuristic procedure for removing holes from a list of polygons.
+  // It works by creating a diagonal from the right-most hole vertex
+  // to some other visible vertex.
+  // Time complexity: O(h*(n^2)), h is the # of holes, n is the # of vertices.
+  // Space complexity: O(n)
+  // params:
+  //    inpolys:
+  //       A list of polygons that can contain holes.
+  //       Vertices of all non-hole polys have to be in counter-clockwise order.
+  //       Vertices of all hole polys have to be in clockwise order.
+  //    outpolys:
+  //       A list of polygons without holes.
+  // Returns 1 on success, 0 on failure.
   int RemoveHoles(TPPLPolyList *inpolys, TPPLPolyList *outpolys);
 
-  //triangulates a polygon by ear clipping
-  //time complexity O(n^2), n is the number of vertices
-  //space complexity: O(n)
-  //params:
-  //   poly : an input polygon to be triangulated
-  //          vertices have to be in counter-clockwise order
-  //   triangles : a list of triangles (result)
-  //returns 1 on success, 0 on failure
+  // Triangulates a polygon by ear clipping.
+  // Time complexity: O(n^2), n is the number of vertices.
+  // Space complexity: O(n)
+  // params:
+  //    poly:
+  //       An input polygon to be triangulated.
+  //       Vertices have to be in counter-clockwise order.
+  //    triangles:
+  //       A list of triangles (result).
+  // Returns 1 on success, 0 on failure.
   int Triangulate_EC(TPPLPoly *poly, TPPLPolyList *triangles);
 
-  //triangulates a list of polygons that may contain holes by ear clipping algorithm
-  //first calls RemoveHoles to get rid of the holes, and then Triangulate_EC for each resulting polygon
-  //time complexity: O(h*(n^2)), h is the number of holes, n is the number of vertices
-  //space complexity: O(n)
-  //params:
-  //   inpolys : a list of polygons to be triangulated (can contain holes)
-  //             vertices of all non-hole polys have to be in counter-clockwise order
-  //             vertices of all hole polys have to be in clockwise order
-  //   triangles : a list of triangles (result)
-  //returns 1 on success, 0 on failure
+  // Triangulates a list of polygons that may contain holes by ear clipping
+  // algorithm. It first calls RemoveHoles to get rid of the holes, and then
+  // calls Triangulate_EC for each resulting polygon.
+  // Time complexity: O(h*(n^2)), h is the # of holes, n is the # of vertices.
+  // Space complexity: O(n)
+  // params:
+  //    inpolys:
+  //       A list of polygons to be triangulated (can contain holes).
+  //       Vertices of all non-hole polys have to be in counter-clockwise order.
+  //       Vertices of all hole polys have to be in clockwise order.
+  //    triangles:
+  //       A list of triangles (result).
+  // Returns 1 on success, 0 on failure.
   int Triangulate_EC(TPPLPolyList *inpolys, TPPLPolyList *triangles);
 
-  //creates an optimal polygon triangulation in terms of minimal edge length
-  //time complexity: O(n^3), n is the number of vertices
-  //space complexity: O(n^2)
-  //params:
-  //   poly : an input polygon to be triangulated
-  //          vertices have to be in counter-clockwise order
-  //   triangles : a list of triangles (result)
-  //returns 1 on success, 0 on failure
+  // Creates an optimal polygon triangulation in terms of minimal edge length.
+  // Time complexity: O(n^3), n is the number of vertices
+  // Space complexity: O(n^2)
+  // params:
+  //    poly:
+  //       An input polygon to be triangulated.
+  //       Vertices have to be in counter-clockwise order.
+  //    triangles:
+  //       A list of triangles (result).
+  // Returns 1 on success, 0 on failure.
   int Triangulate_OPT(TPPLPoly *poly, TPPLPolyList *triangles);
 
-  //triangulates a polygons by firstly partitioning it into monotone polygons
-  //time complexity: O(n*log(n)), n is the number of vertices
-  //space complexity: O(n)
-  //params:
-  //   poly : an input polygon to be triangulated
-  //          vertices have to be in counter-clockwise order
-  //   triangles : a list of triangles (result)
-  //returns 1 on success, 0 on failure
+  // Triangulates a polygon by first partitioning it into monotone polygons.
+  // Time complexity: O(n*log(n)), n is the number of vertices.
+  // Space complexity: O(n)
+  // params:
+  //    poly:
+  //       An input polygon to be triangulated.
+  //       Vertices have to be in counter-clockwise order.
+  //    triangles:
+  //       A list of triangles (result).
+  // Returns 1 on success, 0 on failure.
   int Triangulate_MONO(TPPLPoly *poly, TPPLPolyList *triangles);
 
-  //triangulates a list of polygons by firstly partitioning them into monotone polygons
-  //time complexity: O(n*log(n)), n is the number of vertices
-  //space complexity: O(n)
-  //params:
-  //   inpolys : a list of polygons to be triangulated (can contain holes)
-  //             vertices of all non-hole polys have to be in counter-clockwise order
-  //             vertices of all hole polys have to be in clockwise order
-  //   triangles : a list of triangles (result)
-  //returns 1 on success, 0 on failure
+  // Triangulates a list of polygons by first
+  // partitioning them into monotone polygons.
+  // Time complexity: O(n*log(n)), n is the number of vertices.
+  // Space complexity: O(n)
+  // params:
+  //    inpolys:
+  //       A list of polygons to be triangulated (can contain holes).
+  //       Vertices of all non-hole polys have to be in counter-clockwise order.
+  //       Vertices of all hole polys have to be in clockwise order.
+  //    triangles:
+  //       A list of triangles (result).
+  // Returns 1 on success, 0 on failure.
   int Triangulate_MONO(TPPLPolyList *inpolys, TPPLPolyList *triangles);
 
-  //creates a monotone partition of a list of polygons that can contain holes
-  //time complexity: O(n*log(n)), n is the number of vertices
-  //space complexity: O(n)
-  //params:
-  //   inpolys : a list of polygons to be triangulated (can contain holes)
-  //             vertices of all non-hole polys have to be in counter-clockwise order
-  //             vertices of all hole polys have to be in clockwise order
-  //   monotonePolys : a list of monotone polygons (result)
-  //returns 1 on success, 0 on failure
+  // Creates a monotone partition of a list of polygons that
+  // can contain holes. Triangulates a set of polygons by
+  // first partitioning them into monotone polygons.
+  // Time complexity: O(n*log(n)), n is the number of vertices.
+  // Space complexity: O(n)
+  // params:
+  //    inpolys:
+  //       A list of polygons to be triangulated (can contain holes).
+  //       Vertices of all non-hole polys have to be in counter-clockwise order.
+  //       Vertices of all hole polys have to be in clockwise order.
+  //    monotonePolys:
+  //       A list of monotone polygons (result).
+  // Returns 1 on success, 0 on failure.
   int MonotonePartition(TPPLPolyList *inpolys, TPPLPolyList *monotonePolys);
 
-  //partitions a polygon into convex polygons by using Hertel-Mehlhorn algorithm
-  //the algorithm gives at most four times the number of parts as the optimal algorithm
-  //however, in practice it works much better than that and often gives optimal partition
-  //uses triangulation obtained by ear clipping as intermediate result
-  //time complexity O(n^2), n is the number of vertices
-  //space complexity: O(n)
-  //params:
-  //   poly : an input polygon to be partitioned
-  //          vertices have to be in counter-clockwise order
-  //   parts : resulting list of convex polygons
-  //returns 1 on success, 0 on failure
+  // Partitions a polygon into convex polygons by using the
+  // Hertel-Mehlhorn algorithm. The algorithm gives at most four times
+  // the number of parts as the optimal algorithm, however, in practice
+  // it works much better than that and often gives optimal partition.
+  // It uses triangulation obtained by ear clipping as intermediate result.
+  // Time complexity O(n^2), n is the number of vertices.
+  // Space complexity: O(n)
+  // params:
+  //    poly:
+  //       An input polygon to be partitioned.
+  //       Vertices have to be in counter-clockwise order.
+  //    parts:
+  //       Resulting list of convex polygons.
+  // Returns 1 on success, 0 on failure.
   int ConvexPartition_HM(TPPLPoly *poly, TPPLPolyList *parts);
 
-  //partitions a list of polygons into convex parts by using Hertel-Mehlhorn algorithm
-  //the algorithm gives at most four times the number of parts as the optimal algorithm
-  //however, in practice it works much better than that and often gives optimal partition
-  //uses triangulation obtained by ear clipping as intermediate result
-  //time complexity O(n^2), n is the number of vertices
-  //space complexity: O(n)
-  //params:
-  //   inpolys : an input list of polygons to be partitioned
-  //             vertices of all non-hole polys have to be in counter-clockwise order
-  //             vertices of all hole polys have to be in clockwise order
-  //   parts : resulting list of convex polygons
-  //returns 1 on success, 0 on failure
+  // Partitions a list of polygons into convex parts by using the
+  // Hertel-Mehlhorn algorithm. The algorithm gives at most four times
+  // the number of parts as the optimal algorithm, however, in practice
+  // it works much better than that and often gives optimal partition.
+  // It uses triangulation obtained by ear clipping as intermediate result.
+  // Time complexity O(n^2), n is the number of vertices.
+  // Space complexity: O(n)
+  // params:
+  //    inpolys:
+  //       An input list of polygons to be partitioned. Vertices of
+  //       all non-hole polys have to be in counter-clockwise order.
+  //       Vertices of all hole polys have to be in clockwise order.
+  //    parts:
+  //       Resulting list of convex polygons.
+  // Returns 1 on success, 0 on failure.
   int ConvexPartition_HM(TPPLPolyList *inpolys, TPPLPolyList *parts);
 
-  //optimal convex partitioning (in terms of number of resulting convex polygons)
-  //using the Keil-Snoeyink algorithm
-  //M. Keil, J. Snoeyink, "On the time bound for convex decomposition of simple polygons", 1998
-  //time complexity O(n^3), n is the number of vertices
-  //space complexity: O(n^3)
-  //   poly : an input polygon to be partitioned
-  //          vertices have to be in counter-clockwise order
-  //   parts : resulting list of convex polygons
-  //returns 1 on success, 0 on failure
+  // Optimal convex partitioning (in terms of number of resulting
+  // convex polygons) using the Keil-Snoeyink algorithm.
+  // For reference, see M. Keil, J. Snoeyink, "On the time bound for
+  // convex decomposition of simple polygons", 1998.
+  // Time complexity O(n^3), n is the number of vertices.
+  // Space complexity: O(n^3)
+  // params:
+  //    poly:
+  //       An input polygon to be partitioned.
+  //       Vertices have to be in counter-clockwise order.
+  //    parts:
+  //       Resulting list of convex polygons.
+  // Returns 1 on success, 0 on failure.
   int ConvexPartition_OPT(TPPLPoly *poly, TPPLPolyList *parts);
 };
 
--- test/imageio.h
+++ test/imageio.h
@@ -29,53 +29,58 @@
 #define IMGTYPE_PGM 3
 #define IMGTYPE_UNSUPPORTED 999
 
-//handles basic image file input/output
-//only supports uncompressed image formats
+// Handles basic image file input/output.
+// Only supports uncompressed image formats (RAW, BMP, PPM, PGM).
 class ImageIO {
   protected:
-  //gets the file extension of file filename
+  // Gets the file extension from the file name.
   const char *GetFileExtension(const char *filename);
 
-  //determines the imege format from the filename
+  // Determines the image format from the file name.
   int GetImageType(const char *filename);
 
   public:
-  //loads the image from filename into image
-  //automatically determines the image format
+  // Loads the image from `filename` into `image`.
+  // This method automatically determines the image format.
   void LoadImage(const char *filename, Image *image);
 
-  //loads the image of the specified format (imageType) from filename into image
+  // Loads the image from a file named `filename` into `image`,
+  // using the format given as `imageType`.
   void LoadImage(const char *filename, Image *image, int imageType);
 
-  //saves the image into file named 'filename'
-  //automatically determines the image format
+  // Saves the image into file named `filename`.
+  // This method automatically determines the image format.
   void SaveImage(const char *filename, Image *image);
 
-  //saves the image into file named 'filename', using the format given as imageType
+  // Saves the image into file named `filename`,
+  // using the format given as `imageType`.
   void SaveImage(const char *filename, Image *image, int imageType);
 
-  //loads the uncompressed BMP image from filename into image
+  // Loads the uncompressed BMP image from `filename` into `image`.
   void LoadImageBMP(const char *filename, Image *image);
-  //saves the image into file named 'filename' in uncompressed BMP format
+  // Saves the image into file named `filename` in uncompressed BMP format.
   void SaveImageBMP(const char *filename, Image *image);
 
-  //loads the PPM image from filename into image
+  // Loads the PPM image from `filename` into `image`.
   void LoadImagePPM(const char *filename, Image *image);
-  //saves the image into file named 'filename' in PPM format
+  // Saves the image into file named `filename` in PPM format.
   void SaveImagePPM(const char *filename, Image *image);
 
-  //loads the PGM image from filename into image
+  // Loads the PGM image from `filename` into `image`.
   void LoadImagePGM(const char *filename, Image *image);
-  //saves the image into file named 'filename' in PGM format
+  // Saves the image into file named `filename` in PGM format.
   void SaveImagePGM(const char *filename, Image *image);
 
-  //loads the image from the file named 'filename'
-  //the file is assumed to be structured so that it only contains an array of raw (gray) pixel values
-  //as the file does not contain the image width and height, those are passed as parameters to the function
-  //if width and height are 0, the image is assumed to be square and the width and height are computed based on the file size
+  // Loads the image from the file named `filename`.
+  // The file is assumed to be structured so that it only contains
+  // an array of raw (gray) pixel values, as the file does not contain
+  // the image width and height, those are passed as parameters to the
+  // function. If width and height are 0, the image is assumed to be
+  // square and the width and height are computed based on the file size.
   void LoadImageRAW(const char *filename, Image *image, long width = 0, long height = 0);
 
-  //saves the image to a file named 'filename'
-  //only the array of raw (gray) pixel values is stored, without additional information such as image size
+  // Saves the image to a file named `filename`.
+  // Only the array of raw (gray) pixel values are stored,
+  // without additional information such as image size.
   void SaveImageRAW(const char *filename, Image *image);
 };
